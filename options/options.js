9bb2f901dcdb147ec1e1f117d48306800c352315
Rodrigo <rodrigo@crowdbotics.com>
2022-07-13 18:16:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dmap_11",
+  url: "https://dmap_11.botics.co",
+  api: "https://dmap_11.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
